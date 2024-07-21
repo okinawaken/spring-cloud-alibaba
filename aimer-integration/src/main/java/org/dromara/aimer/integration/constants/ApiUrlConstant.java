880ea584760ec7b6bc3c776d0fb69fa861c5a324
Shinomiya <1138700280@qq.com>
2024-07-22 02:24:35 +0800

    新增API_URL

--- aimer-integration/src/main/java/org/dromara/aimer/integration/constants/ApiUrlConstant.java
+++ aimer-integration/src/main/java/org/dromara/aimer/integration/constants/ApiUrlConstant.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class ApiUrlConstant {
 
+    public static final String API_URL = "https://api.bilibili.com/";
+
     public static final String SHOW_URL = "https://show.bilibili.com/";
 
     public static final String PASSPORT_URL = "https://passport.bilibili.com/";
