6f67364a0d799ce4014a82d7aaf407951efd98e6
Shinomiya <1138700280@qq.com>
2024-07-24 20:25:38 +0800

    验证码

---
+++ aimer-integration/src/main/java/org/dromara/aimer/integration/bilibili/login/response/CaptchaResult.java
@@ -0,0 +1,29 @@
+package org.dromara.aimer.integration.bilibili.login.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+import org.dromara.aimer.integration.bilibili.login.dto.GeetestDTO;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CaptchaResult {
+
+    /**
+     * 验证方式
+     * 用于判断使用哪一种验证方式，目前所见只有极验
+     * geetest：极验
+     */
+    private String type;
+
+    /**
+     * 登录 API token
+     * 与 captcha 无关，与登录接口有关
+     */
+    private String token;
+
+    /**
+     * 极验captcha数据
+     */
+    private GeetestDTO geetest;
+}
