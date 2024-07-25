575a4e39b9427887fe79c21e16ea21452e54ecd6
Shinomiya <1138700280@qq.com>
2024-07-25 16:00:29 +0800

    新增产量

---
+++ aimer-common/src/main/java/org/dromara/aimer/common/constants/HttpHeaderConstant.java
@@ -0,0 +1,9 @@
+package org.dromara.aimer.common.constants;
+
+/**
+ * HTTP状态头
+ */
+public interface HttpHeaderConstant {
+
+    String SET_COOKIE = "Set-Cookie";
+}
--- aimer-service/src/main/java/org/dromara/aimer/service/service/bilibili/interceptor/ReceivedCookiesInterceptor.java
+++ aimer-service/src/main/java/org/dromara/aimer/service/service/bilibili/interceptor/ReceivedCookiesInterceptor.java
@@ -3,6 +3,7 @@ package org.dromara.aimer.service.service.bilibili.interceptor;
 import com.github.lianjiatech.retrofit.spring.boot.interceptor.BasePathMatchInterceptor;
 import okhttp3.Response;
 import org.apache.commons.collections4.CollectionUtils;
+import org.dromara.aimer.common.constants.HttpHeaderConstant;
 import org.dromara.aimer.service.internal.generator.IAccountInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,7 +20,7 @@ public class ReceivedCookiesInterceptor extends BasePathMatchInterceptor {
     @Override
     protected Response doIntercept(Chain chain) throws IOException {
         Response originalResponse = chain.proceed(chain.request());
-        List<String> setCookieHeaders = originalResponse.headers("Set-Cookie");
+        List<String> setCookieHeaders = originalResponse.headers(HttpHeaderConstant.SET_COOKIE);
         if (CollectionUtils.isNotEmpty(setCookieHeaders)) {
             // todo 保存cookie
         }
