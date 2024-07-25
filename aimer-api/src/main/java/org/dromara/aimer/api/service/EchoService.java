397fc622614124996c5ee13cd043eb752a5001f3
Shinomiya <1138700280@qq.com>
2024-07-25 15:51:47 +0800

    rpc测试完毕

--- aimer-api/src/main/java/org/dromara/aimer/api/service/EchoService.java
+++ aimer-api/src/main/java/org/dromara/aimer/api/service/EchoService.java
@@ -3,12 +3,11 @@ package org.dromara.aimer.api.service;
 import org.dromara.aimer.api.constants.ApplicationConstant;
 import org.dromara.aimer.common.response.BaseResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = ApplicationConstant.AIMER_CONSOLE)
 public interface EchoService {
-    @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+    @GetMapping(value = "/echo/{str}")
     BaseResponse<String> echo(@PathVariable("str") String str);
 }
--- aimer-console/src/main/java/org/dromara/aimer/console/controller/AimerController.java
+++ aimer-console/src/main/java/org/dromara/aimer/console/controller/AimerController.java
@@ -1,16 +1,23 @@
 package org.dromara.aimer.console.controller;
 
 import org.dromara.aimer.common.response.BaseResponse;
+import org.dromara.aimer.integration.aimer.EchoRpcService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/echo")
-public class EchoController {
-    @GetMapping(value = "/{str}")
+@RequestMapping("/aimer")
+public class AimerController {
+
+    @Autowired
+    private EchoRpcService echoRpcService;
+
+    @GetMapping(value = "/echo/{str}")
     public BaseResponse<String> echo(@PathVariable String str) {
-        return BaseResponse.ok(str);
+        String result = echoRpcService.echo(str);
+        return BaseResponse.ok(result);
     }
 }
