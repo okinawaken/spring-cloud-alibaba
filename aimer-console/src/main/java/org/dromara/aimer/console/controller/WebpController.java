189d112155999462662838946522881a9b36974f
Shinomiya <1138700280@qq.com>
2024-07-26 17:15:51 +0800

    新增controller

--- aimer-console/src/main/java/org/dromara/aimer/console/controller/WebpController.java
+++ aimer-console/src/main/java/org/dromara/aimer/console/controller/WebpController.java
@@ -1,16 +1,17 @@
 package org.dromara.aimer.console.controller;
 
 import org.dromara.aimer.common.response.BaseResponse;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/webp")
 public class WebpController {
 
-    @GetMapping("/encode")
-    public BaseResponse<Void> encode() {
+    @PostMapping("/encode")
+    public BaseResponse<Void> encode(MultipartFile multipartFile) {
         return BaseResponse.ok();
     }
 }
